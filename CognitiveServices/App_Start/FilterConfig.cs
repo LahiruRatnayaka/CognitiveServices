c5aacc4f0e678535a070ca02610557da3c5ef88a
SilentCoder <lahirunimantha@outlook.com>
2017-10-09 12:36:34 +0530

    Initiate

--- CognitiveServices/App_Start/FilterConfig.cs
+++ CognitiveServices/App_Start/FilterConfig.cs
@@ -5,6 +5,10 @@ namespace CognitiveServices
 {
     public class FilterConfig
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filters"></param>
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
